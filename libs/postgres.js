7ae7f2f77dbdc689967f9a450b42cdac63272b5e
edd0214 <miguel.cfeteit@gmail.com>
2024-04-24 20:42:45 -0600

    Se modificó la integración de node-postgres mediante un Pool para evitar saturar el servidor con las request

--- libs/postgres.js
+++ libs/postgres.js
@@ -1,16 +1,17 @@
-const { Client} = require('pg');
+const { Client} = require('pg'); // llamamos al driver
 
 
-async function getConnection () {
-  const client = new Client ({
-    host: 'localhost',
-    port: 5432,
-    user: 'miguel',
-    password: 'admin123',
-    database: 'my_store'
+async function getConnection () { //creamos una función async para ejecutar el await
+  const client = new Client ({ //creamos una nueva instancia de client
+    host: 'localhost', //indicamos la configuración de la conexión
+    port: 5432, //indocamos el puerto desde el cual se ejecuta la base de datos
+    user: 'miguel',  // señalamos el nombre de usuario
+    password: 'admin123',  //indicamos la contraseña configurada
+    database: 'my_store'  //nombre de la base de datos
   });
-  await client.connect();
-  return client;
+  await client.connect(); // creamos la conexión del cliente
+  return client;  //retornamos el client para poder utilizar y ejecutar consultas
 }
 
-module.exports = getConnection;
+module.exports = getConnection; //
+
